4ecd65c5265d910d4b6fdb79885d70c179057754
Hoang Quoc Huy S3930607 <s3930607@rmit.edu.vn>
2026-09-17 16:42:25 +0700

    re commit all fix html bug

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=D:\projects\Assignment Python\Huy-and-Linh-Python-Assignment\.venv"
+set "VIRTUAL_ENV=C:\Users\admin\Desktop\Final Python Project\Huy-and-Linh-Python-Assignment\.venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
